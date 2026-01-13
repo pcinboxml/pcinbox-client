17ad45f8b18804e275f0129ad828cdfa593b0c69
Alejandro-Paredes-Soto <alejandroparedessoto18@gmail.com>
2026-01-13 13:45:08 -0600

    validacion de longitud de valores de las caracteristicas

--- src/app/result-search-category/SearchCategoryContent.tsx
+++ src/app/result-search-category/SearchCategoryContent.tsx
@@ -623,10 +623,10 @@ const SearchCategoryContent = () => {
                                                       <span className="italic text-[13px]">
                                                         {carac.value &&
                                                         carac?.value?.length >
-                                                          40
+                                                          70
                                                           ? `${carac?.value?.slice(
                                                               0,
-                                                              40
+                                                              70
                                                             )}...`
                                                           : carac?.value}
                                                       </span>
