4693aadd06136b6cc5502770a91b0be654a42806
Alejandro-Paredes-Soto <alejandroparedessoto18@gmail.com>
2026-01-19 18:06:05 -0600

    mejora en la logica de protectedRoute

--- src/app/middleware/protectedRoute.tsx
+++ src/app/middleware/protectedRoute.tsx
@@ -13,7 +13,10 @@ export default function useProtectedRoute(pathname: string) {
       router.replace("/register");
     } else {
       // si hay token y estás en login o register, redirige a home
-      if ((token && pathname === "/principal") || pathname === "/register") {
+      if (
+        (token && pathname === "/principal") ||
+        (token && pathname === "/register")
+      ) {
         router.replace("/principal");
       }
     }
