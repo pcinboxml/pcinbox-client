ba5a10120fbf2dfab25fa246dfef34a9ddfbb2d8
Alejandro-Paredes-Soto <alejandroparedessoto18@gmail.com>
2026-05-23 14:19:48 -0600

    cambios de estilos de barra de productos

--- src/app/components/subMenuProductos/SubMenuProductos.tsx
+++ src/app/components/subMenuProductos/SubMenuProductos.tsx
@@ -38,7 +38,7 @@ const SubMenuProductos = ({
         }}
       >
         <div
-          className={`${style.menu1} h-[100%] max-h-[550px] ${forceVisible ? "w-full" : "w-[150px]"}  overflow-y-auto overflow-x-hidden`}
+          className={`${style.menu1} h-[100%] max-h-[550px] ${forceVisible ? "w-full" : "w-[180px]"}  overflow-y-auto overflow-x-hidden`}
           style={{
             padding: forceVisible ? "10px" : "2px",
           }}
@@ -75,6 +75,7 @@ const SubMenuProductos = ({
                             textAlign: "start",
                             color: "white",
                             fontSize: "12px",
+                            fontWeight: "500",
                           }}
                           // onMouseEnter={() => {
                           //   if (categoria) {
